712d6ff2af5540fed319533a21187048e97d8814
simongarisch <gatman946@gmail.com>
2020-03-03 05:30:46 +1100

    modified observable.py

--- pylookback/assets/observable.py
+++ pylookback/assets/observable.py
@@ -1,24 +1,16 @@
 from weakref import WeakSet
 
 
-OBSERVER_METHOD = "msg_from_observable"
-
-
 class Observable:
     def __init__(self):
         self._observers = WeakSet()
 
     def add_observer(self, observer):
         self._observers.add(observer)
-        if not hasattr(observer, OBSERVER_METHOD):
-            raise AttributeError(
-                "observer requires method %s" % OBSERVER_METHOD
-            )
 
     def remove_observer(self, observer):
         self._observers.discard(observer)
 
     def notify_observers(self):
         for observer in self._observers:
-            method = getattr(observer, OBSERVER_METHOD)
-            method(self)
+            observer.observable_update(self)
--- tests/test_observable.py
+++ tests/test_observable.py
@@ -1,3 +1,38 @@
+from pylookback.assets.observable import Observable
 
-def test_observer_init():
-    pass
+
+class Observer:
+    is_updated = False
+
+    def observable_update(self, observable):
+        self.is_updated = True
+
+
+def test_observable_add_remove():
+    observable = Observable()
+    assert len(observable._observers) == 0
+    observer1 = Observer()
+    observer2 = Observer()
+
+    observable.add_observer(observer1)
+    assert len(observable._observers) == 1
+
+    observable.add_observer(observer2)
+    assert len(observable._observers) == 2
+
+    observable.remove_observer(observer1)
+    assert len(observable._observers) == 1
+    assert list(observable._observers) == [observer2]
+
+    del observer2
+    assert len(observable._observers) == 0  # should be weak set
+
+
+def test_observable_messaging():
+    observer = Observer()
+
+    observable = Observable()
+    observable.add_observer(observer)
+    assert observer.is_updated is False
+    observable.notify_observers()
+    assert observer.is_updated is True
