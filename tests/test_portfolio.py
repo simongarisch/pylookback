301ad7d9e356329c39d662b1a23958833850ba12
simongarisch <gatman946@gmail.com>
2020-03-06 06:45:51 +1100

    adding to portfolio trade tests

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -42,7 +42,7 @@ def test_transfer_stock():
     aapl = Stock("AAPL US", 300, "USD")
     audusd = FxRate("AUDUSD", 0.65)
     portfolio.transfer(aapl, 1000)
-    assert portfolio.value, 2 == 1000 * 2.50 + 1000 * 300 / audusd.rate
+    assert portfolio.value == 1000 * 2.50 + 1000 * 300 / audusd.rate
 
     portfolio.transfer(aapl, -1000)
     assert portfolio.value == 1000 * 2.50  # just zzb left
@@ -76,3 +76,47 @@ def test_stock_observed():
     assert portfolio.value == 1000 * 2.50
     zzb.price = 2
     assert portfolio.value == 1000 * 2
+
+    aapl = Stock("AAPL US", 300, "USD")
+    audusd = FxRate("AUDUSD", 0.65)
+    portfolio.transfer(aapl, 1000)
+    assert portfolio.value == 1000 * 2 + 1000 * 300 / audusd.rate
+    aapl.price = 310
+    assert portfolio.value == 1000 * 2 + 1000 * 310 / audusd.rate
+    audusd.rate = 0.75
+    assert portfolio.value == 1000 * 2 + 1000 * 310 / audusd.rate
+
+
+def test_portfolio_trade():
+    portfolio = Portfolio("AUD")
+    aud = Cash("AUD")
+    portfolio.transfer(aud, 1000)
+    assert portfolio.value == 1000
+
+    assert portfolio.get_holding_units("ZZB AU") == 0
+    zzb = Stock("ZZB AU", 2.50, "AUD")
+    portfolio.trade(zzb, 100)
+    assert portfolio.get_holding_units("ZZB AU") == 100
+    assert portfolio.get_holding_units("AUD") == 750
+    assert portfolio.value == 1000
+
+    aapl = Stock("AAPL US", 300, "USD")
+    audusd = FxRate("AUDUSD", 0.65)
+    portfolio.trade(aapl, 1)
+    assert portfolio.get_holding_units("ZZB AU") == 100
+    assert portfolio.get_holding_units("AUD") == 750
+    assert portfolio.get_holding_units("AAPL US") == 1
+    assert portfolio.get_holding_units("USD") == -300
+
+    assert portfolio.value == 1000  # no prices have moved yet
+    zzb.price = 2.40
+    aapl.price = 310
+    audusd.rate = 0.7
+
+    expected_value = (
+        +2.40 * 100  # zzb stock
+        + 750 * 1  # aud cash
+        + 310 * 1 / 0.7  # aapl stock
+        - 300 * 1 / 0.7  # usd cash
+    )
+    assert round(portfolio.value, 2) == round(expected_value, 2)
